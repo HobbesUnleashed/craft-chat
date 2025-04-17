344cf70a3cf194c1540d2b5ececb94ee58c91853
Shay <sleacy@googlemail.com>
2025-04-17 22:48:16 +0100

    Added Category and Post models

--- chat/settings.py
+++ chat/settings.py
@@ -25,13 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ztc4w)5xdgw)fhuhq^!7h#v#jnq1@ftv(r=m0^jni_7!unaz2h"
+# SECRET_KEY = "django-insecure-ztc4w)5xdgw)fhuhq^!7h#v#jnq1@ftv(r=m0^jni_7!unaz2h"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
+    "127.0.0.1",
 ]
 
 
@@ -89,6 +91,11 @@ WSGI_APPLICATION = "chat.wsgi.application"
 # }
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+]
 
 
 # Password validation
--- crafts/models.py
+++ crafts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+
+
+class Post(models.Model):
+    AGE = (
+        (0, "5+"),
+        (1, "8+"),
+        (2, "10+"),
+        (3, "13+"),
+        (4, "15+"),
+        (5, "18+"),
+    )
+
+    SKILL = (
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 5),
+    )
+
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    materials = models.TextField()
+    time_taken = models.IntegerField(help_text="Time taken in minutes")
+    age = models.IntegerField(choices=AGE, default=0)
+    media_url = models.URLField(max_length=200, blank=True, null=True)
+    skill = models.IntegerField(choices=SKILL, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
