c15aa6430357f7a6f9924283354533a623e6f3dc
Shay <sleacy@googlemail.com>
2025-04-21 15:49:13 +0100

    Update of Media locations

--- chat/settings.py
+++ chat/settings.py
@@ -147,5 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Media upload handlers
-MEDIA_URL = "/media/"
+MEDIA_URL = "https://craft-chat-69c1b6ef7580.herokuapp.com/media/"
 MEDIA_ROOT = BASE_DIR / "media"
